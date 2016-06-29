b7c08509400130bf1598989deb31820e9cdd9c39
Nam Dao <nam.d.dao@gmail.com>
2016-06-29 15:58:39 -0700

    begin intergration with client

--- db/config.js
+++ db/config.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 var MongoClient = require('mongodb').MongoClient;
 var findOrCreate = require('mongoose-findorcreate');
-//var MONGODB_URI = require('./mlabconfig.js');
+require('dotenv').config();
+
 var MONGODB_URI = process.env.MONGODB_URI;
 var db;
+
 
 mongoose.connect(MONGODB_URI, 
   function (err, database) {
@@ -30,7 +32,5 @@ var userSchema = new Schema({
 userSchema.plugin(findOrCreate);
 
 var User = mongoose.model('User', userSchema);
-
-//module.exports.db = db;
 
 module.exports.User = User;
